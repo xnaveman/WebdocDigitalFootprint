c5ea4312cf558b99bf2cf8ec45ed7a061c3ccf3a
Evan Mastrantuono <xnaveman@gmail.com>
2026-02-03 18:19:02 +0100

    first empty template MVP working

--- digitalFootprint/src/elements/Taskbar.jsx
+++ digitalFootprint/src/elements/Taskbar.jsx
@@ -1,5 +1,5 @@
 import './taskbar.css';
-import { CgFolder, CgEye, CgGlobeAlt } from "react-icons/cg";
+import { CgFolder, CgEye, CgGlobeAlt, CgBell, CgLogOff, CgBatteryFull } from "react-icons/cg";
 import { windowsConfig } from '../windowsConfig';
 
 const iconMap = {
@@ -9,20 +9,29 @@ const iconMap = {
 };
 
 function Taskbar({ onOpenWindow }) {
+    const hardcodedConfigs = windowsConfig.slice(0, 3);
+
     return (
         <div className="taskbar">
-            {windowsConfig.map((config) => {
-                const Icon = iconMap[config.icon];
-                return (
-                    <div 
-                        key={config.id} 
-                        className="taskbar-item" 
-                        onClick={() => onOpenWindow(config)}
-                    >
-                        <Icon />
-                    </div>
-                );
-            })}
+            <div className="taskbar-icons">
+                {hardcodedConfigs.map((config) => {
+                    const Icon = iconMap[config.icon];
+                    return (
+                        <div 
+                            key={config.id} 
+                            className="taskbar-item" 
+                            onClick={() => onOpenWindow(config)}
+                        >
+                            <Icon />
+                        </div>
+                    );
+                })}
+            </div>
+            <div className="taskbar-statuses">
+                <div className="taskbar-status"><CgBell></CgBell></div>
+                <div className="taskbar-status"><CgBatteryFull></CgBatteryFull></div>
+                <div className="taskbar-status"><CgLogOff></CgLogOff></div>
+            </div>
         </div>
     );
 }
