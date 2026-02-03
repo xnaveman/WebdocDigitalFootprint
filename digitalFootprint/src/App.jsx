864af49b2a8d3390ffce5971c56877489c42be10
Evan Mastrantuono <xnaveman@gmail.com>
2026-02-03 11:15:06 +0100

    first commit

---
+++ digitalFootprint/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+import Taskbar from './elements/taskbar.jsx';
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button>
+          count is 0
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+      <Taskbar />
+    </>
+  )
+}
+
+export default App
---
+++ digitalFootprint/src/elements/Taskbar.jsx
@@ -0,0 +1,13 @@
+import './taskbar.css';
+
+function Taskbar() {
+    return (
+        <div className="taskbar">
+            <div className="taskbar-item">Item 1</div>
+            <div className="taskbar-item">Item 2</div>
+            <div className="taskbar-item">Item 3</div>
+        </div>
+    );
+}
+
+export default Taskbar;
